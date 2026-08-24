0c2bb7e0cfc147cb71e623b07c917273b627be21
Domagoj <dantic1@algebra.hr>
2026-08-24 20:58:28 +0200

    add check if Minio Bucket exists

--- scripts/03_process_audio.py
+++ scripts/03_process_audio.py
@@ -2,26 +2,37 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from config.config import MINIO_ENDPOINT, MINIO_USER, MINIO_PASSWORD
+from config.config import MINIO_ENDPOINT, MINIO_USER, MINIO_PASSWORD, MINIO_BUCKET
 
 from minio import Minio
 
-client = Minio(
+minio_client = Minio(
     endpoint=MINIO_ENDPOINT,
     access_key=MINIO_USER,
     secret_key=MINIO_PASSWORD,
-    secure=True
+    secure=False
 )
 
 try:
-    buckets = client.list_buckets()
+    buckets = minio_client.list_buckets()
     print("Connected to Minio")
 except Exception as e:
     print(f"Error connecting to Minio: {e}")
     sys.exit(1)
 
-for b in buckets: 
+print("Buckets:")
+for b in buckets:
     print(f"--- {b.name}")
+
+print()
+
+if not minio_client.bucket_exists(MINIO_BUCKET):
+    minio_client.make_bucket(MINIO_BUCKET)
+    print(f"Bucket created: {MINIO_BUCKET}")
+else:
+    print(f"Bucket '{MINIO_BUCKET}' already exists.")
+
+
 
 
 
