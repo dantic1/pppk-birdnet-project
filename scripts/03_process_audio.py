81dbfa9a9cb012da330b9a5a90e697b142e4bc1e
Domagoj <dantic1@algebra.hr>
2026-08-24 21:39:12 +0200

    add location loading add audio files check

--- scripts/03_process_audio.py
+++ scripts/03_process_audio.py
@@ -1,11 +1,16 @@
 import os
 import sys
+import json 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from config.config import MINIO_ENDPOINT, MINIO_USER, MINIO_PASSWORD, MINIO_BUCKET
+from config.config import MINIO_ENDPOINT, MINIO_USER, MINIO_PASSWORD, MINIO_BUCKET, AUDIO_DIR
 
 from minio import Minio
 
+ALLOWED_EXTENSIONS = { ".mp3", ".wav" }
+LOCATIONS_FILE = os.path.join(os.path.dirname(__file__), "..", "config", "locations.json")
+
+#MINIO CLIENT
 minio_client = Minio(
     endpoint=MINIO_ENDPOINT,
     access_key=MINIO_USER,
@@ -13,6 +18,7 @@ minio_client = Minio(
     secure=False
 )
 
+#MINIO CONNECTION 
 try:
     buckets = minio_client.list_buckets()
     print("Connected to Minio")
@@ -26,11 +32,48 @@ for b in buckets:
 
 print()
 
+
+# Check if MINIO BUCKET exits
 if not minio_client.bucket_exists(MINIO_BUCKET):
     minio_client.make_bucket(MINIO_BUCKET)
     print(f"Bucket created: {MINIO_BUCKET}")
 else:
     print(f"Bucket '{MINIO_BUCKET}' already exists.")
+
+print()
+
+# Load locations.json
+
+with open(LOCATIONS_FILE, "r", encoding="utf-8") as f:
+    locations = json.load(f)
+
+print(f"Loaded {len(locations)} locations: {list(locations.keys())}")
+
+# Pass through data/audio folders
+for location_name in sorted(os.listdir(AUDIO_DIR)):
+    location_path = os.path.join(AUDIO_DIR, location_name)
+
+    if not os.path.isdir(location_path):
+        continue
+
+    #Get coordinates for this folder
+    coords = locations.get(location_name)
+    if coords is None:
+        print(f"No coordinations for this location: {location_name}")
+        continue
+        
+    print(f"--- {location_name} (lat={coords['lat']}) (lon={coords['lon']})")
+
+    for filename in sorted(os.listdir(location_path)):
+        _, ext = os.path.splitext(filename)
+        if ext.lower() not in ALLOWED_EXTENSIONS:
+            continue
+        print(f"\taudio: {filename}")
+    
+
+
+
+
 
 
 
